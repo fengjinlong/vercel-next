c6d2c985f5e1e5ea23ef6d4aa9171bdb0ccfa127
fjl <18618166564@163.com>
2025-04-10 14:36:05 +0800

    c1

--- pm2.config.js
+++ pm2.config.js
@@ -2,15 +2,9 @@ module.exports = {
   apps: [
     {
       name: "next-app",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      script: "server.js",
       cwd: "/root/next-app",
-      watch: [".next"],
-      ignore_watch: ["node_modules", ".git"],
-      watch_options: {
-        followSymlinks: false,
-        usePolling: true,
-      },
+      watch: false,
       env: {
         NODE_ENV: "production",
         PORT: 3000,
